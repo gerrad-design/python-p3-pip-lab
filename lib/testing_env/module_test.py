4cbf7af3dc804b7d1f2f42f7db470d4ab46a647b
gerrad <gerrad.meitamei@student.moringaschool.com>
2025-05-20 01:18:53 +0300

    initial commit

--- lib/testing_env/module_test.py
+++ lib/testing_env/module_test.py
@@ -1,18 +1,13 @@
-from versions import (
-    python_version,
-    requests_version,
-    pytest_version
-)
+from lib.versions import python_version, requests_version, pytest_version
+
 
 def test_python_version():
     version_info = python_version()
     assert version_info.major == 3
-    assert version_info.minor == 8
-
+    assert version_info.minor == 12 
 
 def test_requests_version():
-    assert requests_version() == "2.27.1"
-
+    assert requests_version() == "2.31.0" 
 
 def test_pytest_version():
-    assert pytest_version() == "7.1.3"
+    assert pytest_version() == "7.4.4"  
